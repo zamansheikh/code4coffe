9df86f64443e42b93a9cd7560d4ea582454256e4
Zaman Sheikh <zamano6545@gmail.com>
2021-11-06 20:36:00 +0600

    fuck you

---
+++ Project/LinkList.c
@@ -0,0 +1,189 @@
+#include <stdio.h>
+#include <stdlib.h>
+//UserSnippetBy ZAMAN SHEIKH v0.1
+//Global Structure
+struct node
+{
+    int data;
+    struct node *next;
+} * head, *end, *tail;
+
+//Function Prototypes
+void insert();
+void insertOk(int);
+void disSerial();
+void deleteFirst();
+void deleteAll();
+void display();
+
+
+int main()
+{
+home:
+    system("cls");
+    printf("==================Welcome to Nude/Link\n");
+    printf("==================Choise Option for Link Babe\n");
+    printf("==================Choice I for Input\n==================Choice D for Delete\n==================Choice S for Info\n==================Choice E for Exit\n");
+
+    printf("====================================\n");
+    printf("==>: ");
+    int choice = toupper(getch());
+    if (choice == 'I')
+    {
+        insert();
+        goto home;
+    }
+    else if (choice == 'D')
+    {
+        deleteAll();
+        goto home;
+    }
+    else if (choice == 'S')
+    {
+        display();
+        goto home;
+    }
+    else if (choice == 'E')
+    {
+        system("cls");
+        printf("Welcome to boxchod worlds\n");
+    }
+    else goto home;
+}
+
+void insert()
+{
+    int num;
+    printf("How many Node you wanna Insert\n");
+    scanf("%d", &num);
+    insertOk(num);
+    disSerial();
+}
+
+void insertOk(int num)
+{
+    printf("Node is Creating\n");
+    int data = 0;
+    struct node *newNode, *temp;
+    head = (struct node *)malloc(sizeof(struct node));
+    if (head == NULL)
+    {
+        printf("Memory error\n");
+    }
+    else
+    {
+        printf("Enter the data of 1 No Node\n");
+        scanf("%d", &data);
+        head->data = data;
+        head->next = NULL;
+        temp = head;
+        for (int i = 2; i <= num; i++)
+        {
+            newNode = (struct node *)malloc(sizeof(struct node));
+
+            if (newNode == NULL)
+            {
+                printf("Memory Error\n");
+            }
+            else
+            {
+                printf("Enter the data of %d No Node\n", i);
+                scanf("%d", &data);
+                newNode->data = data;
+                newNode->next = NULL;
+                temp->next = newNode;
+                temp = newNode;
+            }
+        }
+    }
+    printf("Link List Created Successfully\n");
+}
+
+void display()
+{
+    struct node *temp;
+    system("cls");
+    printf("********************************\n");
+    disSerial();
+    printf("We are displaying\n");
+    if (head == NULL)
+    {
+        printf("There are no Nude Bainchud\n");
+    }
+    else
+    {
+        temp = head;
+        int count = 1;
+        while (temp != NULL)
+        {
+            printf("Data of No %d Node : ", count);
+            printf("%d\n", temp->data);
+            temp = temp->next;
+            count++;
+        }
+    }
+    getch();
+}
+
+void disSerial()
+{
+    struct node *temp;
+    if (head == NULL)
+    {
+        printf("Empty List\n");
+    }
+    else
+    {
+        temp = head;
+        printf("Total Node: ");
+        int count = 1;
+        while (temp != NULL)
+        {
+            printf("%d ",count);
+            temp = temp->next;
+            count++;
+            
+        }
+        printf("\n");
+    }
+}
+
+deleteAll()
+{
+    back:
+    printf("Available Nodes in Project\n");
+    disSerial();
+    printf("to deleteAll choice D\nFirst for F\nLast for L\nPosition number\n");
+    
+    printf("==>: ");
+    int choice = toupper(getch());
+    if (choice == 'D')
+    {
+        
+        goto back;
+    }
+    else if (choice == 'F')
+    {
+        deleteFirst();
+        goto back;
+    }
+    else if (choice == 'L')
+    {
+        
+    }
+    else if (choice == 'P')
+    {
+        
+    }
+    else goto back;
+}
+
+void deleteFirst()
+{
+    struct node * temp, * del;
+    del = head;
+    temp = head ->next;
+    head = temp;
+    free(del);
+
+}
