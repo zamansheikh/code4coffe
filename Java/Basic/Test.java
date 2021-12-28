a382af5687bced0afb22090a7bd9ad8b80d5d471
Zaman Sheikh <zaman6545@gmail.com>
2021-12-28 17:18:10 +0600

    Before Home

---
+++ Java/Basic/Test.java
@@ -0,0 +1,16 @@
+// Your First Program
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Zaman!"); 
+    }
+}
+
+//print hello world 10 times
+class HelloWorld10Times {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello, World!");
+        }
+    }
+}
