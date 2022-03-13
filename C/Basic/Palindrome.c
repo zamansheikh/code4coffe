d77ffc336cb5c550f8650a8c14b0652a1cd6c197
Zaman Sheikh <zaman6545@gmail.com>
2022-03-13 19:48:19 +0600

    add all:

---
+++ C/Basic/Palindrome.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdbool.h>
+bool palindrome = false;
+bool isPalindrome(int number){
+    int temp = number;
+    int reverse = 0;
+    while(temp != 0){
+        reverse = reverse * 10;
+        reverse = reverse + temp % 10;
+        temp = temp / 10;
+    }
+    if(number == reverse){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+int main(){
+    int number;
+    scanf("%d", &number);
+    for ( int i =number; i>0; i--){
+        if(isPalindrome(i)){
+            palindrome = true;
+            printf("%d\n", i);
+            break;
+        }
+    }
+
+    return 0;
+}
---
+++ Contest/Vjadge/tep.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    const int n;
+    scanf("%d", &n) gh 
+    int age[n];
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", &age[i]);
+    }
+    int j;
+    int count = 0;
+    for (j = 0; j < n; j++)
+    {
+        if (age[j] >= 13 && age[j] <= 19)
+        {
+            count++;
+        }
+    }
+    printf("Teenage: %d", count);
+    return 0;
+}
